86799092760ed4690f0d5dadfb946141161eaaac
Atef Jlassi <atef.jlassi@esprit.tn>
2022-11-05 13:43:32 +0100

    imports

---
+++ src/a/chapter/one/ImportExample.java
@@ -0,0 +1,10 @@
+package a.chapter.one;
+
+import java.util.Random;
+
+public class ImportExample {
+    public static void main(String[] args) {
+        Random r = new Random(); // DOES NOT COMPILE IF WE REMOVE IMPORT STATEMENT
+        System.out.println(r.nextInt(10));
+    }
+}
